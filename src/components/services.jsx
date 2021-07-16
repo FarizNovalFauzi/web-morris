2f382a760372d90bc1b187f5fc6c532ee4df115e
FarizNovalFauzi <novalfauzi66@gmail.com>
2021-07-16 22:54:36 +0700

    commit pertama

---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Organizational History</h2>
+          <p>
+            Berikut ini organisasi yang pernah saya ikuti serta ikut andil dalam mengurus dan mengelola organisasi tersebut.
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-6'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
